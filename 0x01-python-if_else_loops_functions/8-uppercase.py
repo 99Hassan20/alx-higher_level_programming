ec96c1bc280a6f7fcfcec2f49c3d1d9817688ce7
99Hassan20 <hassanoigag0@gmail.com>
2023-08-08 23:41:00 +0100

    another task done

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for letter in str:
+        if ord(letter) >= ord('a') and ord(letter) <= ord('z'):
+            letter = chr(ord(letter) - 32)
+        print("{}".format(letter), end="")
+    print()
+
+
+uppercase("hello 98 World")
