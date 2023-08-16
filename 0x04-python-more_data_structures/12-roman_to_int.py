fae0e49ebc15ba8c348d7cedb89e39ac0e7e7178
99Hassan20 <hassanoigag0@gmail.com>
2023-08-16 23:44:10 +0100

    fixed a bug

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+
+def is_roman_string(str):
+    for letter in str:
+        if letter not in "IVXLCDM":
+            return 0
+    return 1
+
+
 def roman_to_int(roman_string):
-    if not roman_string:
-        return 0    
+    if not roman_string or not is_roman_string(roman_string):
+        return 0
     roman_numbers = {
             'I': 1,
             'V': 5,
@@ -10,7 +18,7 @@ def roman_to_int(roman_string):
             'C': 100,
             'D': 500,
             'M': 1000
-            }
+        }
     sum_numbers = 0
     for i in range(len(roman_string) - 1):
         if roman_numbers[roman_string[i]] < roman_numbers[roman_string[i + 1]]:
