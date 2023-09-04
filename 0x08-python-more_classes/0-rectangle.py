44148978e3f793c7dcf21f8c66c6df65df461563
99Hassan20 <hassanoigag0@gmail.com>
2023-09-04 19:43:52 +0100

    i think its fixed

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,9 @@
-"""this a reactangle class"""
 # !/usr/bin/python3
+"""
+0-rectangle: this is a module that defines a rectangle
+"""
 
 
 class Rectangle:
     """Class that defines a rectangle"""
+    pass
