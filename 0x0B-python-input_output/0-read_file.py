7d3897a8a5a55d4fd6b8a4c35a634efbf65647ce
99Hassan20 <hassanoigag0@gmail.com>
2023-09-12 23:48:07 +0100

    more fixes

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""Read file"""
+"""module to read a file"""
 
 
 def read_file(filename=""):
+    """Read file"""
     with open(filename, encoding='utf-8') as f:
         print(f.read(), end="")
-
-if __name__ == "__main__":
-    read_file("my_file_0.txt")      
