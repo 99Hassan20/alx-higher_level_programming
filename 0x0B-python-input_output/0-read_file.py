6c7d62a05c2b497e5459bdb35f0d16590fd31932
99Hassan20 <hassanoigag0@gmail.com>
2023-09-12 23:46:39 +0100

    first task done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """Read file"""
 
+
 def read_file(filename=""):
     with open(filename, encoding='utf-8') as f:
         print(f.read(), end="")
+
+if __name__ == "__main__":
+    read_file("my_file_0.txt")      
