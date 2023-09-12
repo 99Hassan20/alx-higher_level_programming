0c57278861a3abf67c94982ce9d8ffe897ab7368
99Hassan20 <hassanoigag0@gmail.com>
2023-09-13 00:14:19 +0100

    another task done

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module to handle json"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    with open(filename, encoding='utf-8') as f:
+        return json.load(f)
