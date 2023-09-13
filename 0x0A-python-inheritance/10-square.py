a861b65f6f2b174e2bed0e2c7c1551520036f877
Hassan Oigag <hassanoigag0@gmail.com>
2023-09-13 10:12:22 +0100

    another task done

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,15 +2,12 @@
 """ Module that contains a class Saquare that inherits from BaseGeometry """
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
-class Square(BaseGeometry):
+class Square(Rectangle):
     """ Class that defines a square """
     def __init__(self, size):
         """ Method that initializes the instance """
         self.integer_validator("size", size)
         self.__size = size
-
-    def area(self):
-        """ Method that returns the area of the square """
-        return self.__size ** 2
+        super().__init__(size, size)
