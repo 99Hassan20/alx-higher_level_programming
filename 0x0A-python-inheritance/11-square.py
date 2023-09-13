3d704c5954068fe77bd37792531d79e6adbc13c3
Hassan Oigag <hassanoigag0@gmail.com>
2023-09-13 10:13:11 +0100

    another task done

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module that contains a class Saquare that inherits from BaseGeometry """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """ Class that defines a square """
+    def __init__(self, size):
+        """ Method that initializes the instance """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """ Method that returns the string representation of the square """
+        return "[Square] {}/{}".format(self.__size, self.__size)
