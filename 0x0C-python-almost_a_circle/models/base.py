dccc56538144883bf76bb605525ffb28d25560f4
99Hassan20 <hassanoigag0@gmail.com>
2023-09-18 20:00:21 +0100

    almost a circle first task done

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" Base class for all models"""
+
+
+class Base:
+    """ Base class for all models"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Initialize the base class"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
