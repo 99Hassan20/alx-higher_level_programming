765a9d16f916beb993b8b5f161de624fce86a3ee
99Hassan20 <hassanoigag0@gmail.com>
2023-11-17 22:46:25 +0100

    fixed a another bug

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
                        passwd=_passwd, db=dbname, port=3306)
     cur = db.cursor()
     cur.execute("select * from states where\
-                name like 'N%' order by states.id asc")
+                name like binary 'N%' order by states.id asc")
     rows = cur.fetchall()
     for row in rows:
         print(row)
