862d7b745de7a3c003c58b888e8d6688e3325ff5
99Hassan20 <hassanoigag0@gmail.com>
2023-11-19 20:33:29 +0100

    another task done

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""lists all State objects from the database hbtn_0e_6_usa"""
+
+from model_state import Base, State
+from sys import argv, exit
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("please provide the right args")
+        exit(1)
+    user_name = argv[1]
+    password = argv[2]
+    dbname = argv[3]
+    engine = create_engine(f"mysql\
++mysqldb://{user_name}:{password}\
+@localhost/{dbname}", pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(engine)
+    session = Session()
+    all_states = session.query(State).order_by(State.id.asc()).all()
+    if len(all_states) == 0:
+        print("Nothing")
+    else:
+        print(f"{all_states[0].id}: {all_states[0].name}")
